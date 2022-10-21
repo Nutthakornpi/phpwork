32452779a4be240a75e6a685024c487f230b76cc
nutthakornpi <s64309010008@ptnicec.ac.th>
2022-10-21 11:49:32 +0700

    ส่งไฟล์ comment

---
+++ comment.php
@@ -0,0 +1,19 @@
+<?php
+
+//บรรททัดที่ 6-7 เเสดงผลการใช้งาน comment เเบบบรรทัดเดียว
+echo "Nutthakorn pihhuntod <br>";
+echo "information Technology <br>";
+//echo "pattani <br>";
+#echo "Thailand <br>";
+
+//บรรททัดที่ 6-7 เเสดงผลการใช้งาน comment เเบบหลายบรรทัด
+/*echo "<br>";
+echo "94000 <br>";
+echo "Asia <br>";*/
+echo "Universal";
+
+
+
+
+
+?>
